b07b3795c71b026661c5ef0560dab72bbb6abfde
rsp-rsp <40926875+rsp-rsp@users.noreply.github.com>
2024-02-26 04:25:01 +0000

    completed chapter 3 exercises

--- src/test/java/HairSalonTest.java
+++ src/test/java/HairSalonTest.java
@@ -1,3 +1,4 @@
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class HairSalonTest {
@@ -6,22 +7,26 @@ class HairSalonTest {
 
     @Test
     public void testGetOpeningDays() {
+        String[] expected = {"Monday", "Tuesday"};
+        String[] actual = hairSalon.getOpeningDays();
+        assertArrayEquals(expected, actual);
 
     }
 
     @Test
     public void testSalonIsOpenOnMonday() {
-
+        assertTrue(hairSalon.isOpen("Monday"));
     }
 
     @Test
     public void testSalonIsClosedOnWednesday() {
+        assertFalse(hairSalon.isOpen("Wednesday"));
 
     }
 
     @Test
     public void testExceptionIsThrownIfWeekdayDoesNotEndInDay() {
-
+        Exception e = assertThrows(IllegalArgumentException.class, () -> hairSalon.isOpen("chicken"));
     }
 
 }
